75245d512d3453b3ec9801d9ea49e7443f1703cf
Marco50345 <00114825@uca.edu.sv>
2025-09-24 00:33:13 -0600

    A mimir

--- Saludo.cc
+++ Saludo.cc
@@ -4,5 +4,6 @@ int main()
 {
     std::cout << "Demonos de baja Michelle" << std::endl;
     std::cout << "A huevo" << std::end1;
+    std::cout << "Amonos a mimir Michelle " << std::end1;
     return 0;
 }
